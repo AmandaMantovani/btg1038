17d8e5bee4a9a531b15eb565d83a1062932e698a
amanda.mantovani <amanda.mantovani@luizalabs.com>
2023-09-04 21:31:35 -0300

    OpenClosed

---
+++ POOII Aula/OCP/Esporte.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    internal static class Esporte
+    {
+        //public static string DescreveObjetivo(TipoEsporte tipoEsporte)
+        //{
+        //    if (tipoEsporte == TipoEsporte.Futebol)
+        //    {
+        //        return "O objetivo é fazer gol";
+        //    }
+        //    else if (tipoEsporte == TipoEsporte.Basquete)
+        //    {
+        //        return "O objetivo é fazer cestas!";
+        //    }
+        //    else if (tipoEsporte == TipoEsporte.Volei)
+        //    {
+        //        return "O objetivo é fazer pontos!";
+        //    }
+            
+        //    return "Esporte não identificado!";
+        //}
+
+        public static string DescreveObjetivoFutebol()
+        {
+            return "O objetivo é fazer gol";
+        }
+
+        public static string DescreveObjetivoBasquete()
+        {
+            return "O objetivo é fazer cestas!";
+        }
+
+        public static string DescreveObjetivoVolei()
+        {
+            return "O objetivo é fazer pontos!";
+        }
+
+        public static string DescreveObjetivoCorrida()
+        {
+            return "O objetivo é chegar primeiro";
+        }
+    }
+}
---
+++ POOII Aula/OCP/EsporteFutebol.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OCP
+{
+    internal class EsporteFutebol
+    {
+        private TipoEsporte TipoEsporte { get; set; }
+        public int QuantidadeJogadores { get; set; }
+        public EsporteFutebol()
+        {
+            TipoEsporte = TipoEsporte.Futebol;
+        }
+
+        public string DescreveRegras()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine(Esporte.DescreveObjetivoFutebol());
+            sb.AppendLine($"E isso é feito com {QuantidadeJogadores} jogadores");
+
+            return sb.ToString();
+        }
+    }
+}
